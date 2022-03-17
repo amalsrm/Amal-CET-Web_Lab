d78af95d68ad4fb4aad4c4eb07de4f4ba483afdc
amalsrm <58544519+amalsrm@users.noreply.github.com>
2022-03-17 13:48:11 +0530

    Delete DBMS-lab directory

--- DBMS-lab/company/job_alter.sql
+++
@@ -1,6 +0,0 @@
-use company;
-alter table departments rename to dept;
-alter table employees modify salary smallint;
-alter table employees add Commission int(10);
-select * from dept;
-desc employees;
